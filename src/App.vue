fc40eff6c6e79de677c7309b03834dc8a16d0749
allen-tao <taoqingqiu@gmail.com>
2022-11-03 09:18:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from './components/CommentCard.vue'
+import {comments} from './components/comments'
+</script>
+
+<template>
+  <div class="bg-sky-100 w-full min-h-[100vh] p-4 columns-1 sm:columns-2 md:columns-3 lg:columns-4">
+    <Card 
+      v-for="comment, index in comments"
+      :key="index"
+      :content="comment.content" 
+      :nickname="comment.user.nickname" 
+      :postTime="comment.timeStr" 
+      :likes="comment.likedCount"
+      :avatar="comment.user.avatarUrl"
+      />
+  </div>
+</template>
---
+++ src/components/CommentCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  content: String,
+  nickname: String,
+  avatar: String,
+  likes: Number,
+  postTime: String
+})
+</script>
+
+<template>
+  <div class="break-inside-avoid mb-4 flex flex-col p-4 bg-violet-300 rounded-xl hover:shadow-[0_0_5px_5px_rgba(220,210,260,62)]">
+    <div class="flex items-center text-lg text-slate-500">
+      <img :src="avatar" 
+        class="w-[24px] h-[24px] rounded-xl" />
+      <div class="ml-4">{{nickname}}</div>
+    </div>
+    <div class="text-xl font-bold text-slate-500 my-4">"{{content}}"</div>
+    <div class="text-sm flex justify-between items-center">
+      <div class="text-pink-500">&hearts; {{likes}}</div>
+      
+      <div class="italic text-xs">On {{postTime}}</div>
+    </div>
+  </div>
+</template>
